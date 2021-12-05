bb181fea91778c07fa85bc8f8217a2d35c8c05c0
C110181136 <93572163+C110181136@users.noreply.github.com>
2021-12-05 15:49:15 +0800

    Add files via upload

---
+++ code untie/src/codeuntie.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class codeuntie {
+    public static void main(String[] args) {
+        System.out.println(revHash(6933552791181934L));
+        System.out.println(hash("justdoit")); //574318821802
+    }
+    public static String letters = "cdefghijlmnoqstuvxz";
+    public static String revHash(long hash) {
+        ArrayList<Character> word = new ArrayList<Character>();
+        ArrayList<Character> word2 = new ArrayList<Character>();
+        String word3 = new String();
+        int k = 0;
+        long b;
+        long a = 6933552791181934L;
+        for (int j = 0; j <= 18; j++) {
+            b = (a - j) % 23;
+            if (b == 0) {
+                a = (a - j) / 23;
+                word.add(letters.charAt(j));
+                j = 0;
+                k++;
+            }
+            b = (a - j) % 23;
+            if (b == 0) {
+                a = (a - j) / 23;
+                word.add(letters.charAt(j));
+                j = 0;
+                k++;
+            }
+            if (a <= 7) {
+                break;
+            }
+        }
+        for (int z = k - 1; z >= 0; z--) {
+            word2.add(word.get(z));
+        }
+        for (int h = 0; h < k; h++) {
+            word3 = word3 + word2.get(h);
+        }
+        return word3;
+    }
+        public static long hash(String s){
+            long h = 7;
+            for (int i = 0; i < s.length(); i++) {
+                h = h * 23 + letters.indexOf(s.charAt(i));
+            }
+            return h;
+        }
+    }
